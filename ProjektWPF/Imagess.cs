93414f6b613dfd8540b9cd3c9e2f26f26ed874dd
Adrian Ciepły <adrian648@gmail.com>
2019-02-19 23:44:39 +0100

    wyciągnięcie duplikatów img z folderu

---
+++ ProjektWPF/Imagess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektWPF
+{
+    class Imagess
+    {
+        public string filePath { get; set; }
+        public byte[] hashCode { get; set; }
+    }
+}
--- ProjektWPF/MainWindow.xaml.cs
+++ ProjektWPF/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,7 +15,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
 using WinForms = System.Windows.Forms;
+using System.Drawing.Imaging;
 
 namespace ProjektWPF
 {
@@ -29,19 +32,62 @@ namespace ProjektWPF
         {
             var dialog = new WinForms.FolderBrowserDialog();
 
-            if(dialog.ShowDialog() == WinForms.DialogResult.OK)
+            if (dialog.ShowDialog() == WinForms.DialogResult.OK)
             {
                 appOptions.Visibility = Visibility.Visible;
                 selectedDir.Text = dialog.SelectedPath;
                 DrawImages(dialog.SelectedPath);
-                
+
             }
         }
 
         private void DrawImages(string path)
         {
+            List<Imagess> hashedImages = new List<Imagess>();
+            List<Imagess> DuplicatesList = new List<Imagess>();
             var acceptedFiles = new String[] { "jpg", "jpeg", "png", "bmp", "svg", "gif" };
             var files = GetImages(path, acceptedFiles, false);
+            foreach (var f in files)
+            {
+                byte[] a = HashImg(f);
+                hashedImages.Add(new Imagess { filePath = f, hashCode = a });
+            }
+            CompareToList(hashedImages, DuplicatesList);
+
+            duplicatesTree.Items.Clear();
+            foreach (var d in DuplicatesList)
+            {
+                var item = new TreeViewItem();
+                item.Header = d.filePath;
+                duplicatesTree.Items.Add(item);
+            }
+        }
+
+        private static void CompareToList(List<Imagess> hashedImages, List<Imagess> DuplicatesList)
+        {
+            for (int i = 0; i <= hashedImages.Count; i++)
+            {
+                for (int j = i + 1; j < hashedImages.Count; j++)
+                {
+                    if (CompareImages(hashedImages[i].hashCode, hashedImages[j].hashCode))
+                    {
+                        bool isInList = false;
+                        bool isInList_j = false;
+                        foreach (var d in DuplicatesList)
+                        {
+                            if (d.filePath == hashedImages[i].filePath)
+                                isInList = true;
+                            else if (d.filePath == hashedImages[j].filePath)
+                                isInList_j = true;
+                            else { }
+                        }
+                        if (!isInList)
+                            DuplicatesList.Add(hashedImages[i]);
+                        if (!isInList_j)
+                            DuplicatesList.Add(hashedImages[j]);
+                    }
+                }
+            }
         }
 
         public static String[] GetImages(string folder, String[] filter, bool isRec)
@@ -54,5 +100,38 @@ namespace ProjektWPF
             }
             return files.ToArray();
         }
+
+        public static byte[] HashImg(string fileName)
+        {
+            using (var image = new Bitmap(fileName))
+            {
+                var sha256 = SHA256.Create();
+
+                var rect = new System.Drawing.Rectangle(0, 0, image.Width, image.Height);
+                var data = image.LockBits(rect, ImageLockMode.ReadOnly, image.PixelFormat);
+
+                var dataPtr = data.Scan0;
+
+                var totalBytes = (int)Math.Abs(data.Stride) * data.Height;
+                var rawData = new byte[totalBytes];
+                System.Runtime.InteropServices.Marshal.Copy(dataPtr, rawData, 0, totalBytes);
+
+                image.UnlockBits(data);
+
+                return sha256.ComputeHash(rawData);
+            }
+        }
+        public static bool CompareImages(byte[] img1, byte[] img2)
+        {
+            if (img1 == img2) return true;
+            if (img1 == null || img2 == null) return false;
+            if (img1.Length != img2.Length) return false;
+            for (int i = 0; i < img1.Length; i++)
+            {
+                if (img1[i] != img2[i]) return false;
+            }
+            return true;
+        }
+
     }
 }
